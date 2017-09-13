837166416f1e432e2ebe397834cb147c342e5d5b
ADAMS James <james@medicationforall.com>
2017-09-13 08:38:26 -0400

    Keywords.

--- script/cardContainer/Card.js
+++ script/cardContainer/Card.js
@@ -134,6 +134,15 @@ function Card(){
    */
   this.setKeywords=function(keywords){
       console.log('set keywords for card',keywords);
+      this.data.keywords=keywords;
+      var keywordStore = $('.page').data('keywordStore');
+
+      //set parsed text
+      var kText = keywordStore.findKeywords(keywords);
+      this.node.find('.keywordsList').html(kText);
+
+      //place keywords on back of card.
+      keywordStore.checkKeywords(this.node.find('.front'));
   };
 
 
--- script/editorPane/controls/mixin/HasKeywordListControl.js
+++ script/editorPane/controls/mixin/HasKeywordListControl.js
@@ -1,11 +1,12 @@
 function HasKeywordListControl(){
   this.keywordList = this.node.find('input[name="keywordsList"]');
 
-  this.keywordList.on('change',$.proxy(function(coreNode,event){
+  this.keywordList.on('input',$.proxy(function(coreNode,event){
     coreNode.setKeywords($(this).val());
   },null,this));
 
   this.setKeywords=function(keywords){
+    console.log('attempting to set keywords');
     var cardNode = $('.cardGroup.selected').data('node');
     cardNode.setKeywords(keywords);
   };
